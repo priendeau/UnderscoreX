04ebb7d047d372583fa383058230680251214e68
root <root@UnderscoreXDevlpt.(none)>
2012-09-10 22:28:35 -0400

    correction of setup.py

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ from numpy.distutils.misc_util import Configuration
 ##from UnderscoreX import ObjectGenericAttrHolder
 ##from UnderscoreX import ExceptionGenericAttrMissing
 
+__version__ = '0.0.1r001a-yusut-bozlon'
+
 def configuration(parent_package='',top_path=None):
     config = Configuration('UnderscoreX', parent_package, top_path)
     return config
@@ -43,6 +45,7 @@ if __name__ == "__main__":
     
     if sys.version[:3]>='2.3':
         config['download_url'] = "http://github.com/priendeau/UnderscoreX"
+        config['name'] = "UnderscoreX"
         config['classifiers'] = [
             'Classifier: Development Status :: 4 - Alpha'
             'Classifier: Intended Audience :: Developers'
@@ -58,13 +61,12 @@ if __name__ == "__main__":
 
 
     setup(
-        name="UnderscoreX",
         version=__version__,
         description="This structure is intended for The developper developping pass-thru and conviviable method to intercept attribute on demand.",
         license="BSD NEW LICENCE",
         url="http://github.com/priendeau/UnderscoreX",
         long_description="This structure is intended for The developper developping pass-thru and conviviable method to intercept attribute on demand. By passing information from **kwargs or mutiple-string-chained argument. This allow to build and easy model assuming you had readed the example. It offer a easy chaining method Thru decorator to parse incoming information from **kwargs or any multi-chained string. Once you have defined a correct dictrionary of Attentend Attribute and Messages dedicated to inform the developper.  It contains a mechanism to push into warning or error the missing attribute informaitons. Rather to code individually all the missing attribute and condition, and exception class is provided as example.",
         packages=["UnderscoreX"],
-        keywords          = ['UnderscoreX','kwargs','Kargs','multi-inline','multi-quoted','string'],
+        keywords = ['UnderscoreX','kwargs','Kargs','multi-inline','multi-quoted','string'],
         **config
     )
