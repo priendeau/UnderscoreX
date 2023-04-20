f3b396725322b682282bbc2d8c139f51c64304de
priendeau <maxistedeams@gmail.com>
2023-04-20 02:48:42 -0400

    Adding a home-made bluetooth scanner to scan my Wainlux K6 equiped of bluetooth to respond in Linux world.

---
+++ UnderscoreX/BluetoothK6/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# -*- file : __init__.py -*-
+
+import BluetoothK6
+#from BluetoothK6 import Connection
+from BluetoothK6.Connection import WainluxEngraver as WE
